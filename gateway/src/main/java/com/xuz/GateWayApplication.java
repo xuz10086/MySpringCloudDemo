ecb5b94ad5f552a5f3b788431c6e11836c689406
xuzhe <xuz10086@icloud.com>
2019-04-29 09:01:50 +0800

    路由demo

--- gateway/src/main/java/com/xuz/GateWayApplication.java
+++ gateway/src/main/java/com/xuz/GateWayApplication.java
@@ -1,8 +1,10 @@
 package com.xuz;
 
+import com.xuz.filter.AccessFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +12,10 @@ public class GateWayApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GateWayApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
